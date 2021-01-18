d7c2c9f545c1159691b30494b13e12b210d11c82
Gowri <gowrizrh@gmail.com>
2021-01-18 22:25:06 +1030

    feat: implement partialord and ord for location
    * added tests for the same

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::cell::Cell;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Hello, world!");
@@ -13,9 +14,27 @@ struct Location<'a> {
     parent: Option<&'a Location<'a>>
 }
 
+impl Eq for Location<'_> {}
+
 impl<'a> PartialEq for Location<'a> {
     fn eq(&self, other: &Self) -> bool {
         self.row == other.row && self.col == other.col
+    }
+}
+
+impl<'a> Ord for Location<'a> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let diff: i32 = self.f.get() - other.f.get();
+
+        if diff > 0 { Ordering::Greater }
+        else if diff < 0 { Ordering::Less }
+        else { Ordering::Equal }
+    }
+}
+
+impl<'a> PartialOrd for Location<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -48,4 +67,49 @@ fn location_equality() {
     assert!(origin == origin_duplicate);
     assert!(origin_duplicate != random_point);
     assert!(origin != random_point);
+}
+
+#[test]
+fn location_ordering() {
+    let origin = Location {
+        row: 0,
+        col: 0,
+        f: Cell::new(0),
+        g: Cell::new(0),
+        parent: None
+    };
+
+    let origin_duplicate = Location {
+        row: 0,
+        col: 0,
+        f: Cell::new(5),
+        g: Cell::new(0),
+        parent: None
+    };
+
+    let random_point = Location {
+        row: 10,
+        col: 10,
+        f: Cell::new(10),
+        g: Cell::new(0),
+        parent: None
+    };
+
+    assert!(origin < origin_duplicate);
+    assert!(origin_duplicate < random_point);
+
+    origin.f.set(5);
+
+    // 5 is not less than 5
+    assert!(!(origin < origin_duplicate));
+    assert!(!(origin > origin_duplicate));
+
+    // 5 is less than or equal to 5
+    assert!(origin <= origin_duplicate);
+    assert!(origin >= origin_duplicate);
+
+    random_point.f.set(5);
+
+    assert!(origin <= random_point);
+    assert!(origin >= random_point);
 }
