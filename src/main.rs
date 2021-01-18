65a7cbc3e4d4416486f8bc01a86bcddd58b677d7
Gowri <gowrizrh@gmail.com>
2021-01-18 21:48:59 +1030

    feat: add location struct barebones code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::cell::Cell;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Location<'a> {
+    row: i32,
+    col: i32,
+    f: Cell<i32>,
+    g: Cell<i32>,
+    parent: Option<&'a Location<'a>>
 }
