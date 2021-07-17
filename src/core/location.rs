3a879295cbe0be295341413e86c64b28d62330c3
Gowri <gowrizrh@gmail.com>
2021-07-17 16:17:05 +0930

    :white_check_mark: adding tests

--- src/core/location.rs
+++ src/core/location.rs
@@ -1,4 +1,7 @@
+use std::cell::Cell;
+use std::cmp::Ordering;
 use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
 
 pub enum GridCell {
     Path,
@@ -9,6 +12,42 @@ pub struct Location {
     pub x: usize,
     pub y: usize,
     pub val: GridCell,
+    pub f: Cell<i64>,
+}
+
+impl Eq for Location {}
+
+impl PartialEq for Location {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Hash for Location {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.x.hash(state);
+        self.y.hash(state);
+    }
+}
+
+impl Ord for Location {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let diff: i64 = self.f.get() - other.f.get();
+
+        if diff > 0 {
+            Ordering::Greater
+        } else if diff < 0 {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        }
+    }
+}
+
+impl PartialOrd for Location {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Debug for Location {
@@ -23,5 +62,127 @@ impl Debug for GridCell {
             GridCell::Path => write!(f, "0"),
             _ => write!(f, "1"),
         }
+    }
+}
+
+mod tests {
+    #![allow(unused_imports)]
+    use super::{Cell, GridCell, Location};
+    use std::collections::HashSet;
+
+    #[test]
+    fn location_equality() {
+        let origin = Location {
+            x: 0,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(i64::MAX),
+        };
+
+        let origin_duplicate = Location {
+            x: 0,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(i64::MAX),
+        };
+
+        let random_point = Location {
+            x: 10,
+            y: 150,
+            val: GridCell::Path,
+            f: Cell::new(i64::MAX),
+        };
+
+        // origin is equal to origin duplicate even though they are separate objects
+        assert!(origin == origin_duplicate);
+
+        assert!(origin_duplicate != random_point);
+        assert!(origin != random_point);
+    }
+
+    #[test]
+    fn hash_value_test() {
+        let mut closed: HashSet<&Location> = HashSet::new();
+
+        let a = Location {
+            x: 0,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(i64::MAX),
+        };
+
+        let b = Location {
+            x: 1,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(i64::MAX),
+        };
+
+        let c = Location {
+            x: 0,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(i64::MAX),
+        };
+
+        closed.insert(&a);
+        closed.insert(&b);
+        closed.insert(&c);
+
+        assert_eq!(2, closed.len());
+
+        // Clear hashset
+        closed.clear();
+        assert_eq!(0, closed.len());
+
+        // Case to add multiple references of the same struct
+        closed.insert(&a);
+        closed.insert(&a);
+        closed.insert(&a);
+
+        // Value should be 1, not 3
+        assert_eq!(1, closed.len());
+    }
+
+    #[test]
+    fn location_ordering() {
+        let origin = Location {
+            x: 0,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(0),
+        };
+
+        let origin_duplicate = Location {
+            x: 0,
+            y: 0,
+            val: GridCell::Path,
+            f: Cell::new(5),
+        };
+
+        let random_point = Location {
+            x: 10,
+            y: 10,
+            val: GridCell::Path,
+            f: Cell::new(10),
+        };
+
+        assert!(origin < origin_duplicate);
+        assert!(origin_duplicate < random_point);
+
+        origin.f.set(5);
+
+        // 5 is not less than 5
+        assert!(!(origin < origin_duplicate));
+        assert!(!(origin > origin_duplicate));
+
+        // 5 is less than or equal to 5
+        assert!(origin <= origin_duplicate);
+        assert!(origin >= origin_duplicate);
+
+        random_point.f.set(5);
+
+        assert!(origin <= random_point);
+        assert!(origin >= random_point);
     }
 }
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,17 +1,18 @@
+mod location;
+mod map;
+
+use location::{GridCell, Location};
+use std::cell::Cell;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::BufRead;
 use std::io::BufReader;
 
-
-mod location;
-mod map;
-
 pub fn init() -> Result<(), Box<dyn Error>> {
     let f: File = File::open("./maps/terrain23.txt")?;
     let reader: BufReader<File> = BufReader::new(f);
 
-    let mut map: Vec<Vec<location::Location>> = Vec::new();
+    let mut map: Vec<Vec<Location>> = Vec::new();
 
     for line in reader.lines() {
         map.push(parse_line(line?, map.len()));
@@ -23,20 +24,21 @@ pub fn init() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_line(line: String, current_row: usize) -> Vec<location::Location> {
-    let mut parsed_line: Vec<location::Location> = Vec::new();
+fn parse_line(line: String, current_row: usize) -> Vec<Location> {
+    let mut parsed_line: Vec<Location> = Vec::new();
 
     for c in line.chars() {
-        let block: location::GridCell = match c {
-            '0' => location::GridCell::Path,
-            '1' => location::GridCell::Wall,
+        let block: GridCell = match c {
+            '0' => GridCell::Path,
+            '1' => GridCell::Wall,
             _ => continue,
         };
 
-        let point: location::Location = location::Location {
+        let point: Location = Location {
             x: parsed_line.len(),
             y: current_row,
             val: block,
+            f: Cell::new(0),
         };
 
         parsed_line.push(point);
